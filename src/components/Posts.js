490c356a5520237466375b7a7009da3ff027b088
dMuguti <omiahderek56@gmail.com>
2023-06-08 16:32:33 +0300

    done

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import './post.css';
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const [likeCount, setLikeCount] = useState(0);
+  const [comment, setComment] = useState('');
+  const [showComment, setShowComment] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -28,38 +32,95 @@ const Posts = () => {
     setSelectedPost(post);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const createPost = async () => {
-      try {
-        const response = await fetch('http://localhost:9292/posts', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer YOUR_AUTH_TOKEN', // Replace with your actual authentication token
-          },
-          body: JSON.stringify({ title, body }),
-        });
-        const newPost = await response.json();
-        setPosts((oldPosts) => [...oldPosts, newPost]);
-      } catch (error) {
-        console.error('Failed to create post', error);
-      }
-    };
+  const handleLike = () => {
+    setLikeCount((prevCount) => prevCount + 1);
+  };
 
-    createPost();
+  const handleComment = () => {
+    setShowComment(true);
+  };
+
+  const handleCommentSubmit = () => {
+    setShowComment(false);
+    // Logic to handle the submitted comment, such as sending it to the server or updating the state
+    // You can access the comment value using the `comment` state variable
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:9292/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer YOUR_AUTH_TOKEN', // Replace with your actual authentication token
+        },
+        body: JSON.stringify({ title, body }),
+      });
+      const newPost = await response.json();
+      setPosts((oldPosts) => [...oldPosts, newPost]);
+    } catch (error) {
+      console.error('Failed to create post', error);
+    }
   };
 
   return (
     <div>
+      <div className='header'>
+        <h2>Welcome, Web Wanderer!</h2>
+        <h3>"BLOG"</h3>
+        <p>awesome place to make <br/>oneself productive and entertained through daily updates.</p>
+      </div>
       {selectedPost ? (
-        <div>
-          <h2>{selectedPost.title}</h2>
-          <p>{selectedPost.body}</p>
-          <button onClick={() => setSelectedPost(null)}>Back</button>
+        <div className="post-details">
+          <h2 className="popup-title">{selectedPost.title}</h2>
+          <p className="popup-body">{selectedPost.body}</p>
+          <button onClick={() => setSelectedPost(null)} className="popup-button">
+            Back
+          </button>
+          <div className="like-section">
+            <button onClick={handleLike} className="like-button">
+              Like
+            </button>
+            <p className="like-count">{likeCount} Likes</p>
+          </div>
+          <div className="comment-section">
+            <button onClick={handleComment} className="comment-button">
+              Comment
+            </button>
+            {showComment && (
+              <div className="comment-form">
+                <input
+                  type="text"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  placeholder="Type your comment"
+                />
+                <button onClick={handleCommentSubmit} className="comment-submit">
+                  Submit
+                </button>
+              </div>
+            )}
+            {comment && (
+              <div className="comment-display">
+                <p>Your Comment: {comment}</p>
+              </div>
+            )}
+          </div>
         </div>
       ) : (
         <div>
+          <div className="post-cards">
+            {posts.map((post) => (
+              <div
+                key={post.id}
+                onClick={() => handlePostClick(post)}
+                className="post-card"
+              >
+                <h2>{post.title}</h2>
+              </div>
+            ))}
+          </div>
           <form onSubmit={handleSubmit}>
             <input
               type="text"
@@ -74,13 +135,6 @@ const Posts = () => {
             />
             <button type="submit">Add Post</button>
           </form>
-          <ul>
-            {posts.map((post) => (
-              <li key={post.id} onClick={() => handlePostClick(post)}>
-                <h2>{post.title}</h2>
-              </li>
-            ))}
-          </ul>
         </div>
       )}
     </div>
